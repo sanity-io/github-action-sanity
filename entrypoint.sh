2a5fe433821b308627480fdb0d7da5c347c45cf0
Knut Melvær <knut.melvaer@gmail.com>
2023-08-31 10:16:52 +0200

    Implement PR18

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,6 @@
 
 set -e
 
-sh -c "sanity install && SANITY_AUTH_TOKEN=$SANITY_AUTH_TOKEN sanity $*"
+sh -c "cd $1 && sanity install"
+shift
+sh -c "SANITY_AUTH_TOKEN='$SANITY_AUTH_TOKEN' sanity $*"
